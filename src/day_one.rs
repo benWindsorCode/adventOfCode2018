495abee51f89159f46597cca59619889abc4a858
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2020-06-02 22:37:07 +0100

    Start part two of day one in rust

--- src/day_one.rs
+++ src/day_one.rs
@@ -1,5 +1,4 @@
 use std::fs;
-use std::env;
 
 fn main() {
 
@@ -11,13 +10,28 @@ fn main() {
     let input = input.trim().split('\n');
 
     let mut total = 0;
+    let mut total_history = vec![ total ];
 
     for elem in input {
         let num: i32 = elem.parse().expect("Couldn't parse");
         total = total + num;
+        if val_seen_before(&total_history, total) {
+            println!("Seen before: {}", total);
+            break;
+        };
+        total_history.push(total);
     }
 
     println!("Total: {}", total);
+}
 
+fn val_seen_before(total_history: &Vec<i32>, current_val: i32) -> bool {
+   let seen_before = if total_history.contains(&current_val) {
+        true
+    } else {
+        false
+    };
 
+   println!("Seen before: {} for {}", seen_before, current_val);
+   seen_before
 }
